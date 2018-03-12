0e739126697ad882ce8e4e774e504eddfb69d0cc
dangminhvu <dangminhvu1995@gmail.com>
2018-03-12 10:41:29 +0700

    16

---
+++ JavaScriptTest/16.js
@@ -0,0 +1,15 @@
+function ConvertUnicodeToHyphen(execString){
+	//var charArray = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "{", "}", "[", "]", "|", "\\", "'", ":", ";", "\"", "<", ",", ">", ".", "?", "/", " "];
+	execString = execString.toLowerCase();
+	execString = execString.replace(/[aàáảạãăắằẳẵặâấầẩẫậ]/g,"a");
+	execString = execString.replace(/[eéèẻẽẹêếềểễệ]/g,"e");
+	execString = execString.replace(/[uúùủũụưứừửữự]/g,"u");
+	execString = execString.replace(/[iíìỉĩị]/g,"i");
+	execString = execString.replace(/[oóòỏõọôốồổỗộơớờởỡợ]/g,"o");
+	execString = execString.replace(/[yýỳỷỹỵ]/g,"y");
+	execString = execString.replace(/[đ]/g,"d");
+	execString = execString.replace(/[^a-zA-Z0-9_-]/g,"-");
+	execString = execString.replace(/--+/g,"-");
+	return execString;
+}
+
