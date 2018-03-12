c9ee0d27f32d58412a4443dd6fd4b36a290ade48
dangminhvu <dangminhvu1995@gmail.com>
2018-03-12 10:45:59 +0700

    sua 15

--- JavaScriptTest/15.js
+++ JavaScriptTest/15.js
@@ -1,8 +1,8 @@
-function ConvertUnicodeToHyphen(execString){
+﻿function ConvertUnicodeToHyphen(execString){
 	//var charArray = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "{", "}", "[", "]", "|", "\\", "'", ":", ";", "\"", "<", ",", ">", ".", "?", "/", " "];
 	execString = execString.toLowerCase();
 	execString = execString.replace(/[aàáảạãăắằẳẵặâấầẩẫậ]/g,"a");
-	execString = execString.replace(/[eéèẻẽẹêếềểễệ]/g,"e");
+	
 	execString = execString.replace(/[uúùủũụưứừửữự]/g,"u");
 	execString = execString.replace(/[iíìỉĩị]/g,"i");
 	execString = execString.replace(/[oóòỏõọôốồổỗộơớờởỡợ]/g,"o");
@@ -10,6 +10,7 @@ function ConvertUnicodeToHyphen(execString){
 	execString = execString.replace(/[đ]/g,"d");
 	execString = execString.replace(/[^a-zA-Z0-9_-]/g,"-");
 	execString = execString.replace(/--+/g,"-");
+	execString = execString.replace(/[đ]/g,"d");
 	return execString;
 }
 
